b9c3878b4382a270c63a37fe1a8e68d9a2822a40
virtiz <89717642+virtiz@users.noreply.github.com>
2021-08-28 21:22:14 -0700

    Update Build-Script2.sh

--- Build-Script2.sh
+++ Build-Script2.sh
@@ -10,4 +10,13 @@ export LANG=en_US.UTF-8
 rm -rf /etc/localtime
 hwclock --systohc
 pacman -Sy vim sudo iwd systemd openssh nano networkmanager firefox grub efibootmgr --noconfirm 
-vim /etc/locale.gen
+locale-gen
+echo arch1 > /etc/hostname
+sudo pacman -Syyu
+systemctl enable {iwd.service,sshd.service}
+passwd root
+useradd -m -g users -G wheel -s /bin/bash chris
+passwd chris
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+mkinitcpio -p linux
