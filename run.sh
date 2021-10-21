d95fedaad03dff459d31a46a4ec942e0159c1dfb
virtiz <89717642+virtiz@users.noreply.github.com>
2021-10-21 19:16:28 +0000

    Update run.sh
    cleaned up comments

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -e
 
-######Define Vairables#####
-
-##Set Defaults
+######Define Vairables
 device = "/dev/vda"
 rootsz = "15"
 swapsz = "5"
@@ -17,8 +15,8 @@ echo -n "Enter partition size for Root, Press enter for default (15): "
 read rootsz
 echo -n "Size of Swap? press enter for default (5):  "
 read swapsz
-#this line is a test-delete it
-####Partition Drive####
+
+###Partition the selected Drive
 sudo parted $device mklabel gpt
 sudo parted $device mkpart ESP fat32 1MiB 513MiB
 sudo parted $device set 1 boot on
@@ -28,7 +26,8 @@ sudo parted $device name 2 boot
 sudo parted $device mkpart primary 800MiB 100%
 sudo parted $device name 3 lvm
 sudo parted $device set 3 lvm on
-echo "partitions created"
+
+##Format the LVM partition
 lvmpart=`sudo fdisk -l $device | grep LVM | awk '{print $1}'`
 efipart=`sudo fdisk -l $device | grep EFI | awk '{print $1}'`
 bootpart=`sudo fdisk -l $device | grep filesystem | awk '{print $1}'`
@@ -43,6 +42,8 @@ mkfs.btrfs -L root /dev/lvm/root
 mkfs.btrfs -L home /dev/lvm/home
 mkswap /dev/lvm/swap
 swapon /dev/lvm/swap
+
+##Mount the partitions
 mount /dev/lvm/root /mnt
 mkdir /mnt/{home,boot}
 mount $bootpart /mnt/boot
@@ -50,17 +51,19 @@ mkdir /mnt/boot/efi
 mount $efipart /mnt/boot/efi
 mount /dev/lvm/home /mnt/home
 
-####Setup Configs####
+##install basic packages
 pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr vim btrfs-progs --noconfirm
+
+##update fstab with new mountpoints
 genfstab -U -p /mnt > /mnt/etc/fstab
 
-#Function Buildout
+##Create function called Buildout
 Buildout(){
 #Add multilib mirrors
 sed -i '93s/^#//' /etc/pacman.conf
 sed -i '94s/^#//' /etc/pacman.conf
 
-#Ask Questions
+##Obtain user inputs for initialized variables
 echo -n "Enter Username, Press enter for Default (owner):  "
 read username
 echo -n "Enter HostName, Press enter for Default (Arch-build): "
@@ -68,8 +71,7 @@ read hostname
 echo -n "Enter locale, Press enter for default (America/Phoenix): "
 read locale
 
-
-#Set user preferences
+##Set user preferences
 echo $hostname > /etc/hostname
 ln -s /usr/share/zoneinfo/$locale /etc/localtime
 echo "LANG=$lang" >> /etc/locale.conf
@@ -77,32 +79,33 @@ sed -i '177s/^#//' /etc/locale.gen
 locale-gen
 hwclock --systohc
 
-#Set Passwords
+##Set Passwords
 echo "Enter root password: "
 passwd root
 useradd -m -g users -G wheel $username
 echo "Enter "$username" password: "
 passwd $username
 
-#Install base software packages
+##Install necessary packages
 sudo pacman -Sy && sudo pacman -S networkmanager lvm2 sudo openssh git cifs-utils --noconfirm
 
-#Correct hooks to add lvm2
+##Correct hooks to add lvm2
 sed -i '/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/ i HOOKS=(base udev autodetect modconf block lvm2 filesystems keyboard fsck)' /etc/mkinitcpio.conf
 sed -i '/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/ d' /etc/mkinitcpio.conf
-#Grub install
+
+##Install grub as the bootloader
 grub-install --target=x86_64-efi --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 mkinitcpio -p linux
 
-#Give users access in sudoers
+##Give users access in sudoers
 sed -i '/^# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 sed -i "/root ALL=(ALL)/ i $username ALL=(ALL) ALL" /etc/sudoers
 
-#Enable Services
+##Enable Services
 systemctl enable {sshd,NetworkManager}
 
-#install packages for user
+##install yay for user
 #yay install
 git clone https://aur.archlinux.org/yay.git
 chmod 777 yay
@@ -111,16 +114,14 @@ sudo -u $username makepkg -si --noconfirm
 
 }
 
+##export the function and call it in new environment
 export -f Buildout
 arch-chroot /mnt /bin/bash -c "Buildout"
 
-#cleanup and say goodbye
+##Cleanup and say goodbye
 clear
-echo "yay,openssh,git,NetworkManager have been insalled. You may reboot now"
-echo "would you like to install a Desktop Environment? after rebooting launch ./nextsteps"
-
+echo "Packages (yay,openssh,git,NetworkManager) were successfully insalled."
+echo "Please "exit" and reboot your machine"
 
 #Future NOTES
-#create the DE and start apps scripts and move them to the /home/$username/
-#echo "to install the starter pack run ./starter" && echo "includes: neovim,rofi,brave,zsh,kitty,discord,steam"
-#echo "to install DE run ./plasma ./i3 ./gnome respectively. If you install i3 we recommend installing DE first"
+#create the DE scripts and move them to the /home/$username/
